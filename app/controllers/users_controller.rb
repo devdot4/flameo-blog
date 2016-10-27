859bdae72aa8ce898995731ebf253eaaa2c680cc
dev-4 <dev.4@protonmail.com>
2016-10-27 18:19:03 -0400

    Initial flameo commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @message = "FLAMEO!"
+  end
+  def new
+    @message = "NEW FLAMEO!"
+  end
+  def create
+    # CREATE FLAMEO!
+  end
+  def show
+    @message = "SHOW FLAMEO!"
+  end
+  def edit
+    @message = "EDIT FLAMEO!"
+  end
+  def update
+    # UPDATE FLAMEO!
+  end
+  def destroy
+    # DESTROY FLAMEO!
+  end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    @message = "screw this"
+  end
+end
