c08a501501111d2da284f6a801a3f364fca35678
dev-4 <dev.4@protonmail.com>
2016-10-31 16:53:17 -0400

    Sessions are fully functional

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    
+    if !session[:user_id]
+      @user = "stranger"
+    else
+      @user = User.find(session[:user_id])
+    end
   end
   def about_us
   end
